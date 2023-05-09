5f7532ef062176c0ec2ff9903c3928864d380d83
Gerg-L <GregLeyda@proton.me>
2023-05-08 21:22:10 -0400

    simplified package

--- default.nix
+++ default.nix
@@ -1,23 +1,6 @@
-{
-  lib,
-  rustPlatform,
-}:
-rustPlatform.buildRustPackage rec {
-  pname = "fetch-rs";
-  version = "1.0.0";
-
+{rustPlatform}:
+rustPlatform.buildRustPackage {
+  name = "fetch-rs";
   src = ./.;
-
-  buildInputs = [
-  ];
-
-  cargoSha256 = "sha256-9A1bhXc9gtjpxVfVLyTQ7AEsc3EEBrnBzOuyOBU6T9E=";
-
-  meta = with lib; {
-    homepage = "https://github.com/ISnortPennies/fetch-rs";
-    description = "";
-    license = licenses.unlicense;
-    maintainers = with maintainers; [];
-    platforms = platforms.linux;
-  };
+  cargoLock.lockFile = ./Cargo.lock;
 }
