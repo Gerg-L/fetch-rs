b58b827999c2baab8b718a203b5b0569735be9fe
Gerg-L <GregLeyda@proton.me>
2024-08-04 14:09:25 -0400

    devShell

--- flake.nix
+++ flake.nix
@@ -18,5 +18,18 @@
           fetch-rs = pkgs.callPackage ./package.nix { };
           default = self.packages.x86_64-linux.fetch-rs;
         };
+      devShells.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${pkgs.stdenv.system}.default ];
+            packages = builtins.attrValues {
+              #
+              inherit (pkgs) rust-analyzer rustfmt clippy;
+            };
+          };
+        };
     };
 }
